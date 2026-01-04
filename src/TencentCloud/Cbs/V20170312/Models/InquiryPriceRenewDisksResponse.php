ed4a5e2fca326d10018ab412c9273cf7b5b6a628
tencentcloudapi <tencentcloudapi@tencent.com>
2026-01-05 04:10:23 +0800

    release 3.0.1516

--- src/TencentCloud/Cbs/V20170312/Models/InquiryPriceRenewDisksResponse.php
+++ src/TencentCloud/Cbs/V20170312/Models/InquiryPriceRenewDisksResponse.php
@@ -20,15 +20,15 @@ use TencentCloud\Common\AbstractModel;
 /**
  * InquiryPriceRenewDisks返回参数结构体
  *
- * @method PrepayPrice getDiskPrice() 获取描述了续费云盘的价格。
- * @method void setDiskPrice(PrepayPrice $DiskPrice) 设置描述了续费云盘的价格。
+ * @method PrepayPrice getDiskPrice() 获取<p>描述了续费云盘的价格。</p>
+ * @method void setDiskPrice(PrepayPrice $DiskPrice) 设置<p>描述了续费云盘的价格。</p>
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
 class InquiryPriceRenewDisksResponse extends AbstractModel
 {
     /**
-     * @var PrepayPrice 描述了续费云盘的价格。
+     * @var PrepayPrice <p>描述了续费云盘的价格。</p>
      */
     public $DiskPrice;
 
@@ -38,7 +38,7 @@ class InquiryPriceRenewDisksResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param PrepayPrice $DiskPrice 描述了续费云盘的价格。
+     * @param PrepayPrice $DiskPrice <p>描述了续费云盘的价格。</p>
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
